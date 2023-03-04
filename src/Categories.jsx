eecd774d8bc545f5dfcac3852a262f010c2921ad
Op Pautu <pautuop@gmail.com>
2023-03-04 17:54:24 +0530

    Add non-dynamic menu toggling

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 
-const Categories = () => {
+const Categories = ({ filterItems }) => {
   return (
-    <nav>
-      <div className="btn-container">
-        <button className="filter-btn">all</button>
-        <button className="filter-btn">breakfast</button>
-        <button className="filter-btn">lunch</button>
-        <button className="filter-btn">shakes</button>
-      </div>
-    </nav>
+    <div className="btn-container">
+      <button className="filter-btn" onClick={() => filterItems("all")}>
+        all
+      </button>
+      <button className="filter-btn" onClick={() => filterItems("breakfast")}>
+        breakfast
+      </button>
+      <button className="filter-btn" onClick={() => filterItems("lunch")}>
+        lunch
+      </button>
+      <button className="filter-btn" onClick={() => filterItems("shakes")}>
+        shakes
+      </button>
+    </div>
   );
 };
 
