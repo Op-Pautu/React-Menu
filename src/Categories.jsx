8abf0a681d320b0761605a3b0af9ef3d9cfa48b4
Op Pautu <pautuop@gmail.com>
2023-03-04 17:42:10 +0530

    Add skeloton of the project

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Categories = () => {
-  return <h2>categories component</h2>;
+  return (
+    <nav>
+      <div className="btn-container">
+        <button className="filter-btn">all</button>
+        <button className="filter-btn">breakfast</button>
+        <button className="filter-btn">lunch</button>
+        <button className="filter-btn">shakes</button>
+      </div>
+    </nav>
+  );
 };
 
 export default Categories;
